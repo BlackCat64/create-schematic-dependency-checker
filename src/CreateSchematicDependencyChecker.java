162eb2ed21ffa477afd53244cdc3897520ec93f5
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2025-09-20 17:58:22 +0100

    Added basic error handling

--- src/CreateSchematicDependencyChecker.java
+++ src/CreateSchematicDependencyChecker.java
@@ -1,29 +1,37 @@
 import java.awt.*;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FilenameFilter;
 
 public class CreateSchematicDependencyChecker {
     public static void main(String[] args) {
         Frame frame = new Frame();
+        try {
+            FileDialog dialog = new FileDialog(frame, "Select a Create schematic NBT file", FileDialog.LOAD);
+            dialog.setDirectory(System.getProperty("user.home") + "\\AppData\\Roaming\\.minecraft\\schematics"); // Open the schematics folder in the default minecraft instance
+            dialog.setFilenameFilter((dir, name) -> {
+                return name.toLowerCase().endsWith(".nbt"); // Filter the search for only NBT files
+            });
+            dialog.setVisible(true);
 
-        FileDialog dialog = new FileDialog(frame, "Select a Create schematic NBT file", FileDialog.LOAD);
-        dialog.setDirectory(System.getProperty("user.home") + "\\AppData\\Roaming\\.minecraft\\schematics"); // Open the schematics folder in the default minecraft instance
-        dialog.setFilenameFilter((dir, name) -> {
-            return name.toLowerCase().endsWith(".nbt"); // Filter the search for only NBT files
-        });
-        dialog.setVisible(true);
+            String directory = dialog.getDirectory();
+            String file = dialog.getFile();
 
-        String directory = dialog.getDirectory();
-        String file = dialog.getFile();
+            if (file == null) {
+                throw new FileNotFoundException("No file selected!");
+            }
+            else if (!file.toLowerCase().endsWith(".nbt")) {
+                throw new IllegalArgumentException("File is not an NBT file!");
+            }
 
-        if (file != null) {
-            File selectedFile = new File(directory, file);
-            System.out.println("Schematic: " + selectedFile.getAbsolutePath());
+            File schematicFile = new File(directory, file);
+            System.out.println("Schematic: " + schematicFile.getAbsolutePath());
         }
-        else {
-            System.err.println("No file selected!");
+        catch (Exception e) {
+            e.printStackTrace();
         }
-
-        frame.dispose(); // Close the file chooser when the program finishes
+        finally {
+            frame.dispose(); // Close the file chooser when the program finishes
+        }
     }
 }
