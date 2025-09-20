6b4066cf6a6c0706719fb709dc7903225461f3a6
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2025-09-20 18:31:40 +0100

    Code cleanup

--- src/CreateSchematicDependencyChecker.java
+++ src/CreateSchematicDependencyChecker.java
@@ -6,10 +6,8 @@ import de.pauleff.core.Tag;
 import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FilenameFilter;
 import java.util.*;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class CreateSchematicDependencyChecker {
     public static void main(String[] args) {
@@ -35,46 +33,13 @@ public class CreateSchematicDependencyChecker {
             File schematicFile = new File(directory, file);
             System.out.println("Schematic: " + schematicFile.getAbsolutePath());
 
-            ICompoundTag root = NBTFileFactory.readNBTFile(schematicFile);
-            IListTag blockPalette = root.getList("palette");
-            if (blockPalette == null) {
-                throw new Exception("Invalid NBT format - Could not find block palette.");
-            }
-
-            List<Tag<?>> blocks = blockPalette.getData(); // Get the list out of the IListTag
-            if (blocks == null || blocks.isEmpty()) {
-                throw new Exception("Invalid NBT format - No blocks found.");
-            }
-
-            // for each block tag, get the Name string value and extract the mod ID into a set
-            Set<String> modIDs = new HashSet<>();
-            System.out.println("Processing schematic dependencies...");
-            for (Tag<?> block : blocks) {
-                if (block instanceof ICompoundTag blockTag) {
-                    String blockID = blockTag.getString("Name");
-                    if (blockID == null || blockID.isEmpty()) {
-                        System.err.println("Found empty block ID! Skipping...");
-                        continue;
-                    }
-                    if (blockID.startsWith("minecraft:")) // skip vanilla blocks - minecraft doesn't count as a dependency
-                        continue;
-
-                    String[] tokens = blockID.split(":");
-                    if (tokens.length != 2) {
-                        System.err.println("Found invalid block ID! Skipping..."); // block registry ID must contain a colon separating the mod ID from the block ID
-                        continue;
-                    }
-
-                    modIDs.add(tokens[0]); // add the mod ID to the set
-                }
-                else System.err.println("Found block entry which is not a CompoundTag. Skipping..." );
-            }
+            Set<String> modIDs = getSchematicDependencies(schematicFile); // use helper method to retrieve dependencies of schematic
 
             if (modIDs.isEmpty()) {
                 throw new Exception("Invalid NBT format - No blocks found!");
             }
             else {
-                System.out.println("Processing complete. Found " + modIDs.size() + " mods:\n");
+                System.out.println("Processing complete. Found " + modIDs.size() + " mods.\n");
                 System.out.println("=====DEPENDENCIES======");
 
                 List<String> sortedIDs = new ArrayList<>(modIDs).stream().sorted().toList(); // sort and output the IDs of the mods used in the schematic
@@ -89,5 +54,44 @@ public class CreateSchematicDependencyChecker {
         finally {
             frame.dispose(); // Close the file chooser when the program finishes
         }
+    }
+
+    public static Set<String> getSchematicDependencies(File schematicFile) throws Exception {
+        ICompoundTag root = NBTFileFactory.readNBTFile(schematicFile);
+        IListTag blockPalette = root.getList("palette");
+        if (blockPalette == null) {
+            throw new Exception("Invalid NBT format - Could not find block palette.");
+        }
+
+        List<Tag<?>> blocks = blockPalette.getData(); // Get the list out of the IListTag
+        if (blocks == null || blocks.isEmpty()) {
+            throw new Exception("Invalid NBT format - No blocks found.");
+        }
+
+        // for each block tag, get the Name string value and extract the mod ID into a set
+        Set<String> modIDs = new HashSet<>();
+        System.out.println("Processing schematic dependencies...");
+        for (Tag<?> block : blocks) {
+            if (block instanceof ICompoundTag blockTag) {
+                String blockID = blockTag.getString("Name");
+                if (blockID == null || blockID.isEmpty()) {
+                    System.err.println("Found empty block ID! Skipping...");
+                    continue;
+                }
+                if (blockID.startsWith("minecraft:")) // skip vanilla blocks - minecraft doesn't count as a dependency
+                    continue;
+
+                String[] tokens = blockID.split(":");
+                if (tokens.length != 2) {
+                    System.err.println("Found invalid block ID! Skipping..."); // block registry ID must contain a colon separating the mod ID from the block ID
+                    continue;
+                }
+
+                modIDs.add(tokens[0]); // add the mod ID to the set
+            }
+            else System.err.println("Found block entry which is not a CompoundTag. Skipping..." );
+        }
+
+        return modIDs;
     }
 }
