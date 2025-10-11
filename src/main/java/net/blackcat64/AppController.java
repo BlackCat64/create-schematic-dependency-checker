798c047c94c95507fea158f929e50e760853eed6
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2025-10-11 17:37:04 +0100

    Added initial web controllers and index.html

---
+++ src/main/java/net/blackcat64/AppController.java
@@ -0,0 +1,12 @@
+package net.blackcat64;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AppController {
+    @RequestMapping("/")
+    public String index() {
+        return "index.html";
+    }
+}
