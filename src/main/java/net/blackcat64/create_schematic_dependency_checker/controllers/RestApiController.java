e83f769e5233863fc63d01e361699a8b0fc5c22c
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2025-10-12 14:13:19 +0100

    Testing HTTP response codes

--- src/main/java/net/blackcat64/create_schematic_dependency_checker/controllers/RestApiController.java
+++ src/main/java/net/blackcat64/create_schematic_dependency_checker/controllers/RestApiController.java
@@ -1,6 +1,8 @@
 package net.blackcat64.create_schematic_dependency_checker.controllers;
 
 import net.blackcat64.create_schematic_dependency_checker.SchematicDependencies;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,11 +10,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class RestApiController {
 
-    @RequestMapping("/api/get")
-    public SchematicDependencies getDependencies() {
+    @GetMapping("/schematic")
+    public ResponseEntity<SchematicDependencies> getDependencies() {
         List<String> list = new ArrayList<>(List.of("hello1", "hello2", "hello3"));
-        return new SchematicDependencies("test", list);
+        var dependencies = new SchematicDependencies("test", list);
+//        SchematicDependencies dependencies = null;
+
+        if (dependencies == null) {
+            return ResponseEntity.notFound().build();
+        }
+        else {
+            return ResponseEntity.ok(dependencies);
+        }
     }
 }
