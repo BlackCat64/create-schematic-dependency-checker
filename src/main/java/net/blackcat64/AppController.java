e4c661ed77f29a36ec0336d358b9a54bf763075c
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2025-10-12 13:45:07 +0100

    Added thymeleaf and converted index.html to a view.

--- src/main/java/net/blackcat64/AppController.java
+++ src/main/java/net/blackcat64/AppController.java
@@ -2,6 +2,7 @@ package net.blackcat64;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,7 +11,8 @@ public class AppController {
     private String appName;
 
     @RequestMapping("/")
-    public String index() {
-        return "index.html";
+    public String index(Model model) {
+        model.addAttribute("name", "Danny");
+        return "index";
     }
 }
