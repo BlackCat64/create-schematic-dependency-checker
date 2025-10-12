cc11bf0c35c4b105d2db77956ba88864b42b8e1e
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2025-10-12 14:29:08 +0100

    Added timestamp + formatting to SchematicDependencies DTO

--- src/main/java/net/blackcat64/create_schematic_dependency_checker/controllers/RestApiController.java
+++ src/main/java/net/blackcat64/create_schematic_dependency_checker/controllers/RestApiController.java
@@ -1,11 +1,12 @@
 package net.blackcat64.create_schematic_dependency_checker.controllers;
 
-import net.blackcat64.create_schematic_dependency_checker.SchematicDependencies;
+import net.blackcat64.create_schematic_dependency_checker.dtos.SchematicDependencies;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,7 +17,7 @@ public class RestApiController {
     @GetMapping("/schematic")
     public ResponseEntity<SchematicDependencies> getDependencies() {
         List<String> list = new ArrayList<>(List.of("hello1", "hello2", "hello3"));
-        var dependencies = new SchematicDependencies("test", list);
+        var dependencies = new SchematicDependencies("test", LocalDateTime.now(), list);
 //        SchematicDependencies dependencies = null;
 
         if (dependencies == null) {
--- src/main/java/net/blackcat64/create_schematic_dependency_checker/dtos/SchematicDependencies.java
+++ src/main/java/net/blackcat64/create_schematic_dependency_checker/dtos/SchematicDependencies.java
@@ -1,16 +1,25 @@
-package net.blackcat64.create_schematic_dependency_checker;
+package net.blackcat64.create_schematic_dependency_checker.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
  * Basic class representing a JSON object of a Schematic and its dependencies
+ * Data Transfer Object - used to send data from server to client
  */
 public class SchematicDependencies {
     private String schematicName;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp;
+
     private List<String> dependencies;
 
-    public SchematicDependencies(String schematicName, List<String> dependencies) {
+    public SchematicDependencies(String schematicName, LocalDateTime timestamp, List<String> dependencies) {
         this.schematicName = schematicName;
+        this.timestamp = timestamp;
         this.dependencies = dependencies;
     }
 
@@ -19,6 +28,13 @@ public class SchematicDependencies {
     }
     public void setSchematicName(String schematicName) {
         this.schematicName = schematicName;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
     }
 
     public List<String> getDependencies() {
